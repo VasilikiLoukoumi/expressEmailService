1de96aaacb4562d47c3054d3568686b0e4d500df
Vasiliki Loukoumi <vloukoumi@binarylogic.local>
2018-08-03 16:57:37 +0300

    First commit.

---
+++ app.js
@@ -0,0 +1,40 @@
+﻿const express = require('express');
+const mailer = require('express-mailer');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+
+app.use('/styles', express.static('styles'));
+app.use('/scripts', express.static('scripts'));
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');﻿
+
+
+//install body-parser to use POST
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/', (req, res) => {    
+    res.render(`${__dirname}/views/index`, { qs: req.query });
+    
+});
+
+// POST 
+app.post('/', urlencodedParser, (req, res) => {
+    console.log(req.body);
+    res.render(`${__dirname}/views/form`, { data: req.body });
+ 
+}); 
+
+app.get('/new', (req, res) => {
+    res.render(`${__dirname}/views/form`);
+});
+
+app.get('/feedback', (req, res) => {
+    res.render(`${__dirname}/views/feedback`);
+});
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
+
